c60ff40adc42ae35e6c9d0c177160c81b05024ec
Felix <felix.schmid@festech.de>
2023-01-03 14:23:12 +0100

    fixe header when not loged in

--- projects/rmm/frontend/src/lib/components/Header.svelte
+++ projects/rmm/frontend/src/lib/components/Header.svelte
@@ -8,21 +8,21 @@
     {#if $userStore}
       <Avatar />
       <span class="block ml-2 text-sm">{$userStore?.name}</span>
+	  <Dropdown placement="bottom" triggeredBy="#avatar-menu">
+		<DropdownHeader>
+			<DropdownItem href="/tokens">Tokens</DropdownItem>
+		</DropdownHeader>
+		<DropdownItem
+		  on:click={async () => {
+					localStorage.clear();
+			location.reload();
+		  }}>Sign out</DropdownItem
+		>
+	  </Dropdown>
     {/if}
     {#if !$userStore}
       <Avatar size="md" class="pace-y-2.5 animate-pulse w-8" />
       <span class="pace-y-2.5 w-24 h-4 ml-2 rounded bg-gray-500 animate-pulse" />
     {/if}
   </div>
-  <Dropdown placement="bottom" triggeredBy="#avatar-menu">
-    <DropdownHeader>
-		<DropdownItem href="/tokens">Tokens</DropdownItem>
-    </DropdownHeader>
-    <DropdownItem
-      on:click={async () => {
-				localStorage.clear();
-        location.reload();
-      }}>Sign out</DropdownItem
-    >
-  </Dropdown>
 </Navbar>
