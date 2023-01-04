1655a3907867c2d184cf262cc46526924e9620f8
Felix <felix.schmid@festech.de>
2023-01-04 11:35:28 +0100

    updated deploy frontend script

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 prod="export const apiBase = 'https:\/\/rmm.festech.de\/api';"
-oldFile=$(cat src/vars.ts)
+oldFile=$(cat projects/frontend/src/vars.ts)
 
-echo "$(sed "1s/.*/$prod/1" src/vars.ts)" > src/vars.ts
+echo "$(sed "1s/.*/$prod/1" projects/frontend/src/vars.ts)" > projects/frontend/src/vars.ts
 
 npm run build
-cd projects/frontend
+cd projects/frontend/dist
 
-cd dist
 tar -czf files.tar.gz ./*
 ssh rmm 'cd /var/www/rmm.festech.de && rm -rf ./*'
 scp files.tar.gz rmm:/var/www/rmm.festech.de
@@ -17,3 +16,4 @@ rm files.tar.gz
 
 cd ..
 echo "$oldFile" > src/vars.ts
+cd ../../
