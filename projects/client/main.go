529f19ae95f228e89ac227c7d37c81b0ea45b3e5
Felix <felix.schmid@festech.de>
2023-01-14 23:56:08 +0100

    more structure

--- projects/client/main.go
+++ projects/client/main.go
@@ -48,10 +48,6 @@ func connectWebsocket(url string) {
 	http.SocketConn = c
 	done := make(chan struct{})
 	go system.SendUsage()
-	go func() {
-		time.Sleep(time.Minute * 15)
-		c.Close()
-	}()
 
 	go func() {
 		var msg models.SocketEvent
