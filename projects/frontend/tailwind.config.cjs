bc40a94d61c91478c7dd0d6708f5decb519c1393
Felix <felix.schmid@festech.de>
2023-01-15 00:14:13 +0100

    update color theme

--- projects/frontend/tailwind.config.cjs
+++ projects/frontend/tailwind.config.cjs
@@ -1,17 +1,26 @@
 const config = {
-  content: [
-    "./src/**/*.{html,js,svelte,ts}",
-    "../../node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
-  ],
+    content: ['./src/**/*.{html,js,svelte,ts}', '../../node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
 
-  theme: {
-    extend: {},
-  },
+    theme: {
+        extend: {
+            colors: {
+                gray: {
+                    100: '#F8F8F8',
+                    200: '#E0E0E0',
+                    300: '#C8C8C8',
+                    400: '#888888',
+                    500: '#707070',
+                    600: '#505050',
+                    700: '#383838',
+                    800: '#282828',
+                    900: '#101010',
+                },
+            },
+        },
+    },
 
-  plugins: [
-    require('flowbite/plugin')
-  ],
-  darkMode: 'class',
+    plugins: [require('flowbite/plugin')],
+    darkMode: 'class',
 };
 
 module.exports = config;
