923ff7b0ec244f3e8c692079f7cedb8d549074eb
Felix <felix.schmid@festech.de>
2023-02-04 11:26:54 +0100

    update delete permissions

--- projects/frontend/src/lib/components/DevicePermissions.svelte
+++ projects/frontend/src/lib/components/DevicePermissions.svelte
@@ -18,13 +18,7 @@
         queryFn: () =>
             fetchWithToken(`${apiBase}/devices/${device.id}/permissions`, {
                 method: 'GET',
-            }).then((res) =>
-                res
-                    .json()
-                    .then((data) =>
-                        data.filter((permission: DevicePermission) => permission.user.id !== $userStore.id),
-                    ),
-            ),
+            }).then((res) => res.json()),
     });
 
     const users = createQuery<User[], Error>({
@@ -49,7 +43,7 @@
 
     const handleDelete = async (permission: DevicePermission) => {
         if ((await customConfirm('Are you sure you want to delete this permission?')) === false) return;
-        const res = await fetchWithToken(`${apiBase}/devices/permissions/${permission.id}`, {
+        const res = await fetchWithToken(`${apiBase}/devices/${device.id}/permissions/${permission.id}`, {
             method: 'DELETE',
         });
         if (res.status === 200) {
@@ -86,22 +80,25 @@
             }),
         });
         client.invalidateQueries(['permissions', device.id]);
-
         drawerClosed = true;
     };
 
-	$: filterdUsers = $users.data?.filter((user) => {
-		const permission = $permissions.data?.find((permission) => permission.user.id === user.id);
-		return permission === undefined && user.id !== $userStore.id;
-	});
+    $: filterdUsers = $users.data?.filter((user) => {
+        const permission = $permissions.data?.find((permission) => permission.user.id === user.id);
+        return permission === undefined && user.id !== $userStore.id;
+    });
+
+    $: canChange = $permissions.data?.find((permission) => permission.user.id === $userStore.id)?.changePermissions;
 </script>
 
-<div class="flex w-full justify-end mb-2">
-    <Button on:click={() => (drawerClosed = false)}>
-        <Fa icon={faPlus} class="mr-2" />
-        Add
-    </Button>
-</div>
+{#if canChange}
+    <div class="flex w-full justify-end mb-2">
+        <Button on:click={() => (drawerClosed = false)}>
+            <Fa icon={faPlus} class="mr-2" />
+            Add
+        </Button>
+    </div>
+{/if}
 {#if $permissions.isLoading}
     <div class="text-center">
         <Spinner size="12" />
@@ -110,74 +107,84 @@
 {#if $permissions.data}
     <Accordion>
         {#each $permissions.data as permission (permission.id)}
-            <AccordionItem class="flex justify-between items-center">
-                <span slot="header">
-                    {permission.user.name}
-                </span>
-                <div class="flex flex-row relative">
-                    <div class="absolute right-2 cursor-pointer" on:click={() => handleDelete(permission)}>
-                        <Fa icon={faTrash} />
-                    </div>
-                    <div class="w-1/2">
-                        <Toggle class="my-1" on:click={() => handleToggle(permission)} bind:checked={permission.run}
-                            >Run</Toggle
-                        >
-                        <Toggle class="my-1" on:click={() => handleToggle(permission)} bind:checked={permission.kill}
-                            >Kill</Toggle
-                        >
-                        <Toggle class="my-1" on:click={() => handleToggle(permission)} bind:checked={permission.reboot}
-                            >Reboot</Toggle
-                        >
-                        <Toggle
-                            class="my-1"
-                            on:click={() => handleToggle(permission)}
-                            bind:checked={permission.shutdown}>Shutdown</Toggle
-                        >
-                        <Toggle
-                            class="my-1"
-                            on:click={() => handleToggle(permission)}
-                            bind:checked={permission.processList}>ProcessList</Toggle
-                        >
-                        <Toggle
-                            class="my-1"
-                            on:click={() => handleToggle(permission)}
-                            bind:checked={permission.changePermissions}>ChangePermission</Toggle
-                        >
-                    </div>
-                    <div class="w-1/2">
-                        <Toggle
-                            class="my-1"
-                            on:click={() => handleToggle(permission)}
-                            bind:checked={permission.serviceList}>ServiceList</Toggle
-                        >
-                        <Toggle
-                            class="my-1"
-                            on:click={() => handleToggle(permission)}
-                            bind:checked={permission.serviceStart}>ServiceStart</Toggle
-                        >
-                        <Toggle
-                            class="my-1"
-                            on:click={() => handleToggle(permission)}
-                            bind:checked={permission.serviceStop}>ServiceStop</Toggle
-                        >
-                        <Toggle
-                            class="my-1"
-                            on:click={() => handleToggle(permission)}
-                            bind:checked={permission.serviceRestart}>ServiceRestart</Toggle
-                        >
-                        <Toggle
-                            class="my-1"
-                            on:click={() => handleToggle(permission)}
-                            bind:checked={permission.serviceStatus}>ServiceStatus</Toggle
-                        >
-                        <Toggle
-                            class="my-1"
-                            on:click={() => handleToggle(permission)}
-                            bind:checked={permission.serviceLogs}>ServiceLogs</Toggle
-                        >
-                    </div>
-                </div>
-            </AccordionItem>
+            {#if permission.user.id !== $userStore.id}
+                <AccordionItem class="flex justify-between items-center">
+                    <span slot="header">
+                        {permission.user.name}
+                    </span>
+                    {#if canChange}
+                        <div class="flex flex-row relative">
+                            <div class="absolute right-2 cursor-pointer" on:click={() => handleDelete(permission)}>
+                                <Fa icon={faTrash} />
+                            </div>
+                            <div class="w-1/2">
+                                <Toggle
+                                    class="my-1"
+                                    on:click={() => handleToggle(permission)}
+                                    bind:checked={permission.run}>Run</Toggle
+                                >
+                                <Toggle
+                                    class="my-1"
+                                    on:click={() => handleToggle(permission)}
+                                    bind:checked={permission.kill}>Kill</Toggle
+                                >
+                                <Toggle
+                                    class="my-1"
+                                    on:click={() => handleToggle(permission)}
+                                    bind:checked={permission.reboot}>Reboot</Toggle
+                                >
+                                <Toggle
+                                    class="my-1"
+                                    on:click={() => handleToggle(permission)}
+                                    bind:checked={permission.shutdown}>Shutdown</Toggle
+                                >
+                                <Toggle
+                                    class="my-1"
+                                    on:click={() => handleToggle(permission)}
+                                    bind:checked={permission.processList}>ProcessList</Toggle
+                                >
+                                <Toggle
+                                    class="my-1"
+                                    on:click={() => handleToggle(permission)}
+                                    bind:checked={permission.changePermissions}>ChangePermission</Toggle
+                                >
+                            </div>
+                            <div class="w-1/2">
+                                <Toggle
+                                    class="my-1"
+                                    on:click={() => handleToggle(permission)}
+                                    bind:checked={permission.serviceList}>ServiceList</Toggle
+                                >
+                                <Toggle
+                                    class="my-1"
+                                    on:click={() => handleToggle(permission)}
+                                    bind:checked={permission.serviceStart}>ServiceStart</Toggle
+                                >
+                                <Toggle
+                                    class="my-1"
+                                    on:click={() => handleToggle(permission)}
+                                    bind:checked={permission.serviceStop}>ServiceStop</Toggle
+                                >
+                                <Toggle
+                                    class="my-1"
+                                    on:click={() => handleToggle(permission)}
+                                    bind:checked={permission.serviceRestart}>ServiceRestart</Toggle
+                                >
+                                <Toggle
+                                    class="my-1"
+                                    on:click={() => handleToggle(permission)}
+                                    bind:checked={permission.serviceStatus}>ServiceStatus</Toggle
+                                >
+                                <Toggle
+                                    class="my-1"
+                                    on:click={() => handleToggle(permission)}
+                                    bind:checked={permission.serviceLogs}>ServiceLogs</Toggle
+                                >
+                            </div>
+                        </div>
+                    {/if}
+                </AccordionItem>
+            {/if}
         {/each}
     </Accordion>
 {/if}
@@ -194,13 +201,13 @@
     <List>
         {#if filterdUsers.length > 0}
             {#each filterdUsers as user (user.id)}
-                    <Li class="flex justify-between items-center my-2">
-                        <span>{user.name}</span>
-                        <Button on:click={() => addPermissions(user)} class="ml-2">
-                            <Fa icon={faPlus} class="mr-2" />
-                            Add
-                        </Button>
-                    </Li>
+                <Li class="flex justify-between items-center my-2">
+                    <span>{user.name}</span>
+                    <Button on:click={() => addPermissions(user)} class="ml-2">
+                        <Fa icon={faPlus} class="mr-2" />
+                        Add
+                    </Button>
+                </Li>
             {/each}
         {/if}
     </List>
